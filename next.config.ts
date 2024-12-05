08da5204e313642fbd050e299cee115d3ee1090f
okao <hamzathanees@gmail.com>
2024-12-05 20:54:34 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	// experimental: {
+	// 	serverComponentsExternalPackages: ['grammy'],
+	// },
+	serverExternalPackages: ['grammy'],
 };
 
 export default nextConfig;
